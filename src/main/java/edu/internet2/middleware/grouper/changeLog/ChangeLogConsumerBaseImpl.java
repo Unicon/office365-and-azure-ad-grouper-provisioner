966fa2afbf50c6370fc2d65c267df484bcac27d7
William G. Thompson, Jr <wgthom@gmail.com>
2016-02-14 20:55:54 -0500

    WIP ChangeLogConsumerBaseImpl - mapping methods and executing processing loop

--- src/main/java/edu/internet2/middleware/grouper/changeLog/ChangeLogConsumerBaseImpl.java
+++ src/main/java/edu/internet2/middleware/grouper/changeLog/ChangeLogConsumerBaseImpl.java
@@ -1,6 +1,7 @@
 package edu.internet2.middleware.grouper.changeLog;
 
 import edu.internet2.middleware.grouper.*;
+import edu.internet2.middleware.grouper.app.loader.GrouperLoaderConfig;
 import edu.internet2.middleware.grouper.attr.AttributeDef;
 import edu.internet2.middleware.grouper.attr.AttributeDefName;
 import edu.internet2.middleware.grouper.attr.AttributeDefType;
@@ -23,9 +24,9 @@ public class ChangeLogConsumerBaseImpl extends ChangeLogConsumerBase {
     private static final Logger LOG = LoggerFactory.getLogger(ChangeLogConsumerBaseImpl.class);
 
     /** Maps supported changeLogEntry category and action to methods */
-    enum changeLogEventType {
+    enum ChangeLogEventType {
         group_addGroup {
-            public void process(ChangeLogEntry changeLogEntry, ChangeLogConsumerBaseImpl changeLogConsumerBaseImpl) throws Exception {
+            public void process(ChangeLogEntry changeLogEntry, ChangeLogConsumerBaseImpl changeLogConsumerBaseImpl) {
                 // does this event pertain to us? was the group or one of its parent folders marked for sync
                 final String groupName = changeLogEntry.retrieveValueForLabel(ChangeLogLabels.GROUP_ADD.name);
                 if (changeLogConsumerBaseImpl.groupMarkedForSync(groupName)) {
@@ -34,7 +35,14 @@ public class ChangeLogConsumerBaseImpl extends ChangeLogConsumerBase {
                     // skipping changeLogEntry that doesn't pertain to us
                 }
             }
-        }
+        };
+
+        /**
+         * Placeholder method for mapping category_action to methods.
+         * @param changeLogEntry
+         * @param changeLogConsumerBaseImpl
+         */
+        public abstract void process(ChangeLogEntry changeLogEntry, ChangeLogConsumerBaseImpl changeLogConsumerBaseImpl);
     }
 
     // If syncAttribute was applied to group or one of the parent folders return true
@@ -45,13 +53,13 @@ public class ChangeLogConsumerBaseImpl extends ChangeLogConsumerBase {
             return groupMarkedForSync || shouldSyncFolder(group.getParentStem());
 
         } catch (GroupNotFoundException gnfe) {
-            // Group gone missing before we had a chance to sync
+            // Group gone missing before we had a chance to sync?
             LOG.debug("changeLog.consumer.'{}': grouper group {} removed before we had a chance to sync", consumerName, groupName);
             return false;
         }
     }
 
-    // If syncAttribute applied to folder or parent folder(s) return true
+    // If syncAttribute applied to folder or any parent folder(s) return true
     private boolean shouldSyncFolder(Stem folder) {
         boolean folderMarkedForSync = folder.getAttributeDelegate().retrieveAssignments(syncAttribute).isEmpty();
         return folderMarkedForSync || shouldSyncFolder(folder.getParentStem());
@@ -60,7 +68,18 @@ public class ChangeLogConsumerBaseImpl extends ChangeLogConsumerBase {
 
     private String consumerName;
 
+    /** Name of marker attribute defined in changeLog.consumer.<consumerName>.syncAttributeName */
+    private String syncAttributeName;
     private AttributeDefName syncAttribute;
+
+    public static String ATTRIBUTE_CONFIG_FOLDER_NAME = "etc:attribute";
+    public static String CONFIG_FOLDER_NAME = "changeLogConsumer";
+
+    /** Property name for marker attribute defined in changeLog.consumer.<consumerName>.syncAttributeName */
+    public static String SYNC_ATTRIBUTE_NAME = "syncAttributeName";
+
+
+
 
     /**
      *
@@ -83,6 +102,31 @@ public class ChangeLogConsumerBaseImpl extends ChangeLogConsumerBase {
         LOG.error("ChangeLogConsumerBaseImpl expects to subclassed.");
     }
 
+    /**
+     * expected to be overidden
+     * @param consumerName
+     * @return
+     */
+    protected boolean isFullSyncRunning(String consumerName) {
+        return false;
+    }
+
+
+    /**
+     * Refreshes consumer configuration from grouper-loader.properties each time the consumer is run
+     *
+     * @param consumerName
+     */
+    private void initProperties(String consumerName) {
+        final String consumerConfigPrefix = "changeLog.consumer." + consumerName + ".";
+
+        GrouperLoaderConfig config = GrouperLoaderConfig.retrieveConfig();
+
+        LOG.debug("changeLog.consumer.{}: setting properties...", consumerName);
+
+        syncAttributeName = config.propertyValueStringRequired(consumerConfigPrefix + SYNC_ATTRIBUTE_NAME);
+
+    }
 
     /**
      * Process the list of changeLogEntries since the last time this consumer was run.
@@ -100,76 +144,115 @@ public class ChangeLogConsumerBaseImpl extends ChangeLogConsumerBase {
             consumerName = changeLogProcessorMetadata.getConsumerName();
         }
 
+        // Get config for the consumer from changeLog.consumer.<consumerName>.*
+        initProperties(consumerName);
+
         // syncAttribute name configured in grouper-loader.properties
         // e.g. changeLog.consumer.<consumerName>.<syncAttributeName> = o365
         if (syncAttribute == null) {
-            AttributeDefName attrDefName = AttributeDefNameFinder.findByName(consumerName + SYNC_ATTRIBUTE_NAME, false);
-            if (attrDefName == null) {
+            syncAttribute = AttributeDefNameFinder.findByName(ATTRIBUTE_CONFIG_FOLDER_NAME + ":" + CONFIG_FOLDER_NAME + ":" + syncAttributeName, false);
+            if (syncAttribute == null) {
                 // missing sync attribute, so let's create it
-                Stem syncConfigFolder = StemFinder.findByName(GrouperSession.staticGrouperSession(), SYNC_CONFIG_FOLDER);
-                if (syncConfigFolder == null) {
-                    final Stem etcAttributeStem = StemFinder.findByName(GrouperSession.staticGrouperSession(), ATTRIBUTE_CONFIG_STEM, false);
-                    googleStem = etcAttributeStem.addChildStem(GOOGLE_PROVISIONER, GOOGLE_PROVISIONER);
+                // first check for etc:attribute:changeLogConsumer, and create if missing
+                Stem configFolder = StemFinder.findByName(GrouperSession.staticGrouperSession(), ATTRIBUTE_CONFIG_FOLDER_NAME + ":" + CONFIG_FOLDER_NAME, false);
+                if (configFolder == null) {
+                    final Stem etcAttributeFolder = StemFinder.findByName(GrouperSession.staticGrouperSession(), ATTRIBUTE_CONFIG_FOLDER_NAME, false);
+                    configFolder = etcAttributeFolder.addChildStem(CONFIG_FOLDER_NAME, CONFIG_FOLDER_NAME);
                 }
-
-                AttributeDef syncAttrDef = AttributeDefFinder.findByName(SYNC_TO_GOOGLE_NAME + "Def", false);
+                // next check for attribute definition etc:attribute:changeLogConsumer:<syncAttributeName>AttributeDef, and create if missing
+                AttributeDef syncAttrDef = AttributeDefFinder.findByName(syncAttributeName + "Def", false);
                 if (syncAttrDef == null) {
-                    LOG.info("Google Apps Consumer '{}' - {} AttributeDef not found, creating it now", consumerName, SYNC_TO_GOOGLE + "Def");
-                    syncAttrDef = googleStem.addChildAttributeDef(SYNC_TO_GOOGLE + "Def", AttributeDefType.attr);
+                    LOG.info("changeLog.consumer.{}: - attribute definition not found for {}, creating it now", consumerName, syncAttributeName + "Def");
+                    syncAttrDef = configFolder.addChildAttributeDef(syncAttributeName + "Def", AttributeDefType.attr);
                     syncAttrDef.setAssignToGroup(true);
                     syncAttrDef.setAssignToStem(true);
                     syncAttrDef.setMultiAssignable(true);
                     syncAttrDef.store();
                 }
 
-                LOG.info("Google Apps Consumer '{}' - {} attribute not found, creating it now", consumerName, SYNC_TO_GOOGLE_NAME + consumerName);
-                attrDefName = googleStem.addChildAttributeDefName(syncAttrDef, SYNC_TO_GOOGLE + consumerName, SYNC_TO_GOOGLE + consumerName);
+                // finally create the attribute etc:attribute:changeLogConsumer:<syncAttributeName>. This is the marker attribute for the consumer.
+                syncAttribute = configFolder.addChildAttributeDefName(syncAttrDef, syncAttributeName, syncAttributeName);
+                LOG.info("changeLog.consumer.{}:  attribute name created for {}", consumerName, syncAttributeName);
+            }
+        }
+
+        // change log sequence to return. will be updated in grouper db to keep track of processing progress.
+        long changeLogEntrySequenceNumber = -1;
+
+        // ContextId groups multiple changeLogEntries into a logical change set
+        // tracking for performance purposes. is this really needed?
+        String currentChangeLogEntryContextId = null;
+
+        for (ChangeLogEntry changeLogEntry : changeLogEntryList) {
+
+            // TODO bail out as soon as we can determine this change doesn't apply to us
+            // is the earliest we can tell?
+            // only supporting provisioing attributes on groups and folders (i.e. groups within folders)?
+            // capture all group and membership create/updates/delete for marked groups and folders
+            // is this a group or membership event?
+
+            changeLogEntrySequenceNumber = changeLogEntry.getSequenceNumber();
+
+            // If full sync is running abort this run and try again later
+            if (isFullSyncRunning(consumerName)) {
+                return changeLogEntrySequenceNumber - 1;
             }
 
-            syncAttribute = attrDefName;
+            // if first time through or new contextID, update the contextId
+            if (currentChangeLogEntryContextId == null || !changeLogEntry.getContextId().equals(currentChangeLogEntryContextId)) {
+                currentChangeLogEntryContextId = changeLogEntry.getContextId();
+            }
 
-            return attrDefName;
-                }
+            try {
+                // process the changeLogEntry
+                processChangeLogEntry(changeLogEntry);
+            } catch (Exception e){
+                String message = "changeLog.consumer." + consumerName + ": an error occurred processing sequence number " + changeLogEntrySequenceNumber;
+                LOG.error(message, e);
+                changeLogProcessorMetadata.registerProblem(e, message, changeLogEntrySequenceNumber);
+                changeLogProcessorMetadata.setHadProblem(true);
+                changeLogProcessorMetadata.setRecordException(e);
+                changeLogProcessorMetadata.setRecordExceptionSequence(changeLogEntrySequenceNumber);
+                // TODO then break on retryOnError = false? or just keep processing and let full sync clean it up?
             }
 
         }
 
-        // initConnector(consumerName)?
+        if (changeLogEntrySequenceNumber == -1) {
+            throw new RuntimeException("changeLog.consumer." + consumerName + ": unable to process any records");
+        }
 
+        LOG.debug("changeLog.consumer.{}: finished processing changeLogEntryList. Last sequence number processed was {}", consumerName, changeLogEntrySequenceNumber);
+
+        // the last changeLogEntrySequence processed
+        return changeLogEntrySequenceNumber;
+    }
+
+    /**
+     * Call the method of the {@link ChangeLogEventType} enum which matches the {@link ChangeLogEntry} category and action (i.e. the change log event type).
+     *
+     * @param changeLogEntry the change log entry
+     */
+    private void processChangeLogEntry(ChangeLogEntry changeLogEntry) {
+
+        // construct key from changeLogEntry in the form of <category>_<action>
+        final String changeLogEventTypeKey = changeLogEntry.getChangeLogType().getChangeLogCategory() + "_"
+                + changeLogEntry.getChangeLogType().getActionName();
+
+        ChangeLogEventType changeLogEventType = null;
+
+        // look up method to map to call it
         try {
-            for (ChangeLogEntry changeLogEntry : changeLogEntryList) {
-
-                // TODO bail out as soon as we can determine this change doesn't apply to us
-                // is the earliest we can tell?
-                // only supporting provisioing attributes on groups and folders (i.e. groups within folders)?
-                // capture all group and membership create/updates/delete for marked groups and folders
-
-                // is this a group or membership event?
-
-
-
-            }
-        } catch (Exception e) {
-
+            changeLogEventType = ChangeLogEventType.valueOf(changeLogEventTypeKey);
+            LOG.info("changeLog.consumer.{}: start processing {} for {}", new Object[]{consumerName, changeLogEventTypeKey, changeLogEntry.toStringDeep()});
+            changeLogEventType.process(changeLogEntry, this);
+            LOG.info("changeLog.consumer.{}: end processing {} for {}", new Object[]{consumerName, changeLogEventTypeKey, changeLogEntry.toStringDeep()});
+        } catch (IllegalArgumentException e) {
+            LOG.debug("changeLog.consumer.{}: unsupported {} for {}", new Object[]{consumerName, changeLogEventTypeKey, changeLogEntry.toStringDeep()});
         }
-
-
-        long lastProcessedChangeLogEntryId = -1;
-
-
-
-
-
-
-
-
-
-
-        return 0;
     }
 
     // TODO implement change log type mapping to methods
     // TODO in implement class override methods for specific functionality?
-    // TODO or some poor man's injections for composing functionality?
 
 }
