b0fc103ddb6522baa84910bed1a0aff773cc6d77
William G. Thompson, Jr <wgthom@gmail.com>
2016-02-15 22:58:51 -0500

    WIP added more event types

--- src/main/java/edu/internet2/middleware/grouper/changeLog/consumer/PrintChangeLogConsumer.java
+++ src/main/java/edu/internet2/middleware/grouper/changeLog/consumer/PrintChangeLogConsumer.java
@@ -7,17 +7,44 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Created by thompsow on 2/14/16.
+ * Example change log consumer based on ChangeLogConsumerBaseImpl. ChangeLogConsumerBaseImpl handles
+ * the mapping of change log event to methods, the processing loop, and exception handling.
  */
 public class PrintChangeLogConsumer extends ChangeLogConsumerBaseImpl {
 
     private static final Logger LOG = LoggerFactory.getLogger(PrintChangeLogConsumer.class);
 
     @Override
-    protected void addGroup(ChangeLogEntry changeLogEntry) {
+    protected void addGroup(ChangeLogEntry changeLogEntry, String consumerName) {
         final String groupName = changeLogEntry.retrieveValueForLabel(ChangeLogLabels.GROUP_ADD.name);
-        LOG.debug("changeLog.consumer.{}: addGroup {}", consumerName, groupName);
+        LOG.debug("{} add group {}.", consumerName, groupName);
         // final edu.internet2.middleware.grouper.Group group = connector.fetchGrouperGroup(groupName);
+    }
+
+    @Override
+    protected void updateGroup(ChangeLogEntry changeLogEntry, String consumerName){
+        final String groupName = changeLogEntry.retrieveValueForLabel(ChangeLogLabels.GROUP_UPDATE.name);
+        LOG.debug("{} update group {}.", consumerName, groupName);
+    }
+
+    @Override
+    protected void deleteGroup(ChangeLogEntry changeLogEntry, String consumerName){
+        final String groupName = changeLogEntry.retrieveValueForLabel(ChangeLogLabels.GROUP_DELETE.name);
+        LOG.debug("{} delete group {}.", consumerName, groupName);
+    }
+
+    @Override
+    protected void addMembership(ChangeLogEntry changeLogEntry, String consumerName){
+        final String groupName = changeLogEntry.retrieveValueForLabel(ChangeLogLabels.MEMBERSHIP_ADD.groupName);
+        final String subjectId = changeLogEntry.retrieveValueForLabel(ChangeLogLabels.MEMBERSHIP_ADD.subjectId);
+        LOG.debug("{} add {} to group {}.", new Object[] {consumerName, subjectId, groupName});
+    }
+
+    @Override
+    protected void deleteMembership(ChangeLogEntry changeLogEntry, String consumerName){
+        final String groupName = changeLogEntry.retrieveValueForLabel(ChangeLogLabels.MEMBERSHIP_DELETE.groupName);
+        final String subjectId = changeLogEntry.retrieveValueForLabel(ChangeLogLabels.MEMBERSHIP_DELETE.subjectId);
+        LOG.debug("{} remove {} from group {}.", new Object[] {consumerName, subjectId, groupName});
     }
 
 }
